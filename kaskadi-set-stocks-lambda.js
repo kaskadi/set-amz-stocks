179124562037a20f38a5ea330cafcc7540211fd6
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-10 18:40:00 +0200

    Updated lambda to work with MWS data

--- kaskadi-set-stocks-lambda.js
+++ kaskadi-set-stocks-lambda.js
@@ -4,34 +4,16 @@ module.exports.handler = async (event) => {
     token: process.env.ES_SECRET,
     url: process.env.ES_ENDPOINT
   })
-  const { stockData, warehouse } = event
-  const body = createBulkBody(stockData, warehouse)
+  const { stockData, warehouse, provider } = event
+  const body = createBulkBody(stockData, warehouse, provider)
   await es.bulk({
     refresh: true,
     body
   })
 }
 
-function createBulkBody(stockData, warehouse) {
-  const bulkBody = stockData.flatMap(data => {
-    const op = {
-      update: {
-        _id: data.id,
-        _index: "products"
-      }
-    }
-    let doc = {
-      doc: {
-        stocks: {}
-      }
-    }
-    doc.doc.stocks[warehouse] = {
-      amount: data.quantity,
-      condition: data.condition || ''
-    }
-    return [op, doc]
-  })
-  return bulkBody.concat([
+function createBulkBody(stockData, warehouse, provider) {
+  return processStockData(stockData, warehouse, provider).concat([
     {
       update: {
         _id: warehouse,
@@ -44,4 +26,50 @@ function createBulkBody(stockData, warehouse) {
       }
     }
   ])
+}
+
+function processStockData(stockData, warehouse, provider) {
+  return stockData.flatMap(data => {
+    let op = {
+      update: {
+        _id: data.id,
+        _index: 'products'
+      }
+    }
+    let body = {}
+    const stockInfo = {
+      amount: data.quantity,
+      condition: data.condition || ''
+    }
+    if (provider === 'amz') {
+      op = {
+        updateByQuery: {
+          _index: 'products',
+          _refresh: true
+        }
+      }
+      body = {
+        script: {
+          lang: 'painless',
+          source: `ctx._source['stocks'][${warehouse}] = { asin: ${data.id}, stock: ${JSON.stringify(stockInfo)} }`
+        },
+        query: {
+          filtered: {
+            filter: {
+              term: {}
+            }
+          }
+        }
+      }
+      body.query.filtered.filter.term[`asin.${warehouse.split('_')[1].toUpperCase()}`] = data.id
+    } else {
+      body = {
+        doc: {
+          stocks: {}
+        }
+      }
+      body.doc.stocks[warehouse] = stockInfo
+    }
+    return [op, body]
+  })
 }
