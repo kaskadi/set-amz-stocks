b5161b817f425c60a842b2c9880fe74f23bde492
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-29 12:56:52 +0200

    Fixed wrong usage of .then keyword

--- set-stocks.js
+++ set-stocks.js
@@ -1,8 +1,7 @@
 module.exports.handler = async (event) => {
   const createBulkBody = require('./helpers/create-bulk-body.js')
   const setStocks = require('./helpers/set-stocks.js')
-  return await createBulkBody(event.responsePayload)
-  .then(setStocks)
+  return await setStocks(createBulkBody(event.responsePayload))
   .then(res => {
     console.log(JSON.stringify(res, null, 2))
     return res
