3c6b00eeb60217e6534c0d62a9094c6dcab0ad5e
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-14 11:58:36 +0200

    Fixed indentation

--- set-amz-stocks.js
+++ set-amz-stocks.js
@@ -3,17 +3,17 @@ module.exports.handler = async (event) => {
   const setStocks = require('./helpers/set-es-stocks.js')
   console.log(JSON.stringify(event, null, 2))
   return await setStocks(createBulkBody(event.detail.responsePayload.stockData))
-  .then(res => {
-    console.log(`Errors: ${res.body.errors}`)
-    const { items } = res.body
-    const successes = items.filter(item => item.update.status === 200)
-    const fails = items.filter(item => item.update.status !== 200)
-    console.log(JSON.stringify(successes, null, 2))
-    console.log(JSON.stringify(fails, null, 2))
-    return res
-  })
-  .catch(err => {
-    console.log(JSON.stringify(err, null, 2))
-    return err
-  })
+    .then(res => {
+      console.log(`Errors: ${res.body.errors}`)
+      const { items } = res.body
+      const successes = items.filter(item => item.update.status === 200)
+      const fails = items.filter(item => item.update.status !== 200)
+      console.log(JSON.stringify(successes, null, 2))
+      console.log(JSON.stringify(fails, null, 2))
+      return res
+    })
+    .catch(err => {
+      console.log(JSON.stringify(err, null, 2))
+      return err
+    })
 }
